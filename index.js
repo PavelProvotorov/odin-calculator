27725ebb632c1ebd6a207ea7a550fd1e3e7d21df
Logodum <15616120+PavelProvotorov@users.noreply.github.com>
2023-09-03 16:04:49 +0300

    Added basic JS - Added broilerplate add, subtract, divide and multiply functions - Added basic event listening for all calculator buttons - Added custom data types for calculator buttons, to store value and type of button - Other small changes

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 console.log("<Script started>")
+
+const CURRENT_VALUE = document.getElementById("__currentValue__")
+const PREVIOUS_VALUE = document.getElementById("__previousValue__")
+const SELECTED_OPERATOR = document.getElementById("__selectedOperator__")
+const ACTIVE_BUTTONS = document.querySelectorAll(".active")
+
+// READY
+
+// FUNCTIONS
+function add(a, b) {
+	return a + b
+};
+
+function subtract(a, b) {
+    return a - b
+};
+
+function multiply (a, b) {
+    return a * b
+};
+
+function divide(a, b) {
+    return a / b
+};
+
+function calculate(operator) {
+    return operator
+}
+
+// EVENTS
+ACTIVE_BUTTONS.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        const element = event.target
+        const element_type = element.getAttribute("data-type")
+        const element_value = element.getAttribute("data-value")
+    });
+});
+
+// NOTE: Max legth is 12 symbols
